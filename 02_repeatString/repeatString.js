5b05f882ea7693791f9ffe63a4262af5afb784c5
nelly <nellybulatova12@gmail.com>
2024-05-27 22:11:12 +0300

    add solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num >= 0) {
+        let z = []
+        for (let i =0; i < num; i++) {
+            z.push(string)
+        }
+        return z.join("")
+    } else {
+        return "ERROR"
+    }
 
 };
 
