5f0357ca6ec1cdbfc10765c5a27c5d69cec56fcb
nelly <nellybulatova12@gmail.com>
2024-05-30 09:15:30 +0300

    add solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let orig = []
+    let newString = string.toLowerCase().replace(/[.,!\s]/g, "").split("")
+    
+    newString.forEach(i => orig.push(i))
+    
+    newString.reverse()
+   
+    return newString.join() === orig.join()
 };
-
 // Do not edit below this line
 module.exports = palindromes;
+
